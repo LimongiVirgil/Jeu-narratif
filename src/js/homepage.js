1cf9cb9fe59f7c4e3499550878b3a970721a082c
LimongiVirgil <limongi.virgil@gmail.com>
2019-02-01 23:10:46 +0100

    add image and folder

---
+++ src/js/homepage.js
@@ -0,0 +1,15 @@
+var story1 = document.querySelector('.image .image__story--v1');
+var story2 = document.querySelector('.image .image__story--v2');
+
+setInterval(function() {
+  if (story1.className === 'image__story--v1 image__story') {
+    story1.classList.remove('image__story');
+    story2.classList.add('image__story');
+    console.log('oui')
+  } else {
+    console.log('non')
+    story2.classList.remove('image__story');
+    story1.classList.add('image__story');
+  }
+}, 1000);
+
