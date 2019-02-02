1e49039f3d3e0281bc4582a156cdabb9adf2494f
LimongiVirgil <limongi.virgil@gmail.com>
2019-02-02 19:31:03 +0100

    add story section

---
+++ src/js/histoire.js
@@ -0,0 +1,12 @@
+var story1 = document.querySelector('.image .image__story--chateau1');
+var story2 = document.querySelector('.image .image__story--chateau2');
+
+setInterval(function() {
+  if (story1.className === 'image__story--chateau1 image__story') {
+    story1.classList.remove('image__story');
+    story2.classList.add('image__story');
+  } else {
+    story2.classList.remove('image__story');
+    story1.classList.add('image__story');
+  }
+}, 500);
