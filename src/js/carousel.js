800df6613616c3a2e9975fc11a7667bd01fd26b2
LimongiVirgil <limongi.virgil@gmail.com>
2019-02-05 19:59:04 +0100

    add folders

---
+++ src/js/carousel.js
@@ -0,0 +1,10 @@
+var left = document.querySelector('slider__arrow slider__arrow--left');
+var right = document.querySelector('slider__arrow slider__arrow--right');
+
+left.addEventListener('click', function() {
+  console.log('oui')
+});
+
+right.addEventListener('click', function() {
+  console.log('non')
+});
