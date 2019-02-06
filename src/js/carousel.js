6bab63d858c236f67567342a67e731b934a94f95
LimongiVirgil <limongi.virgil@gmail.com>
2019-02-06 20:24:20 +0100

    add carousel

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,10 +1,24 @@
-var left = document.querySelector('slider__arrow slider__arrow--left');
-var right = document.querySelector('slider__arrow slider__arrow--right');
-
-left.addEventListener('click', function() {
-  console.log('oui')
-});
+let left = document.querySelector('.slider__arrow.slider__arrow--left');
+var right = document.querySelector('.slider__arrow.slider__arrow--right');
+var images = document.querySelectorAll('.image img');
+var index = 0;
 
 right.addEventListener('click', function() {
-  console.log('non')
+  if (index >= 3) {
+    index = 2
+  }
+  images[index].classList.add('image__story');
+  index++;
+  images[index].classList.remove('image__story');
 });
+
+left.addEventListener('click', function() {
+  if (index <= 0) {
+    index = 1
+  }
+  images[index].classList.add('image__story');
+  index--;
+  images[index].classList.remove('image__story');
+});
+
+
