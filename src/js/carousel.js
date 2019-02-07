8b95b84b50d685a7f2fef0343a57a1a7540e0c37
LimongiVirgil <limongi.virgil@gmail.com>
2019-02-07 14:19:55 +0100

    add modification to the carousel

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -3,22 +3,34 @@ var right = document.querySelector('.slider__arrow.slider__arrow--right');
 var images = document.querySelectorAll('.image img');
 var index = 0;
 
+left.classList.add('display');
+
 right.addEventListener('click', function() {
-  if (index >= 3) {
-    index = 2
-  }
+  if (index >= 2) {
+    index = 2;
+  };
   images[index].classList.add('image__story');
   index++;
   images[index].classList.remove('image__story');
+  if (index >= 3) {
+    right.classList.add('display');
+  } else {
+    left.classList.remove('display');
+  }
 });
 
 left.addEventListener('click', function() {
   if (index <= 0) {
-    index = 1
+    index = 1;
   }
   images[index].classList.add('image__story');
   index--;
   images[index].classList.remove('image__story');
+  if (index > 0) {
+    right.classList.remove('display');
+  } else {
+    left.classList.add('display')
+  }
 });
 
 
